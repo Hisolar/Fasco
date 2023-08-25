e6809a5fda369c7455897b94b5119c1990a5035f
Babatunde Wahab Ishola <Solar@SolarMac.local>
2023-08-25 16:39:04 +0100

    first initialization

---
+++ script.js
@@ -0,0 +1,8 @@
+let menuButton = document.getElementById("menuButton")
+let menuContainer = document.getElementById("menuContainer")
+
+function showToggle(){
+    menuContainer.classList.toggle("show")
+}
+
+menuButton.addEventListener("click", showToggle)
